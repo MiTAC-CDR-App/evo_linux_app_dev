b8e7ae5c577dacea5da75b441b5bc8402b4a8d4c
huah.tu <huah.tu@mic.com.tw>
2024-03-01 10:04:25 +0800

    [MQTT] Revise Windows batch file for ADB push.

--- examples/mqtt/uploadAndRun.bat
+++ examples/mqtt/uploadAndRun.bat
@@ -2,6 +2,8 @@ adb root
 adb shell mkdir data/mqttTest
 adb shell mkdir data/mqttTest/cert
 adb push project/MqttTest data/mqttTest/MqttTest
-adb push cert data/mqttTest/cert
+adb push cert/039e1ece-ed28-4507-8c88-09dc7b6fa780.key data/mqttTest/cert
+adb push cert/039e1ece-ed28-4507-8c88-09dc7b6fa780.pem data/mqttTest/cert
+adb push cert/ca-certificates.crt data/mqttTest/cert
 adb shell "cd data/mqttTest;chmod a+x MqttTest;./MqttTest"
 
