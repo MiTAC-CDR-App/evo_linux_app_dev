2b99287048470c7fdba992c2b046d7231fbb58e0
huah.tu <huah.tu@mic.com.tw>
2024-04-12 15:06:51 +0800

    [CamSDK] Rename camera1.0 to camera1.x.

--- sdk/camera1.0/examples/SimpleRecorder/pushAndRun.bat
+++
@@ -1,6 +0,0 @@
-adb root
-adb shell mkdir /data/test
-adb push ../../project/libMitacCameraSdk.so /data/test
-adb push project/SimpleRecorder /data/test
-adb shell "cd /data/test;chmod a+x SimpleRecorder;export LD_LIBRARY_PATH=/data/test;./SimpleRecorder"
-adb pull /data/test/test.mp4
--- sdk/camera1.0/tests/FpsTest/pushAndRun.bat
+++
@@ -1,5 +0,0 @@
-adb root
-adb shell mkdir /data/test
-adb push ../../project/libMitacCameraSdk.so /data/test
-adb push project/FpsTest /data/test
-adb shell "cd /data/test;chmod a+x FpsTest;export LD_LIBRARY_PATH=/data/test;./FpsTest"
